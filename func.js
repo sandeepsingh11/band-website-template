5f649be2ac1503b2c502e58e5bdbdb0febbcc70c
Sandeep Singh <serperior2@gmail.com>
2019-01-06 20:36:13 -0500

    site structure and style update

--- func.js
+++ func.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+	
+	// sticky nav
+	var navbar = $('#navbar');
+	$('#hero').css('margin-top', $(navbar).css('height'));
+	$(window).scroll(function() {
+
+
+
+		// var scrollPosition = $(document).scrollTop();
+
+		// if (scrollPosition >= parseInt(navHeight)) {
+		// 	$('#nav').css('position', 'fixed');
+		// }
+		// else {
+		// 	$('#nav').css('position', 'absolute');
+		// }
+	});
+
+	// set tour hero size
+	var tourContentHeight = $('#tourContent').css('height');
+	var tourContentPadding = $('#tourContent').css('padding-top');
+	$('#tourHero').css('height', (parseInt(tourContentHeight) + parseInt(tourContentPadding)) );
+
 	// hero carousel
 	$('#heroCarousel').slick({
 		dots: false,
@@ -7,10 +30,12 @@ $(document).ready(function() {
 		autoplay: true,
 		autoplaySpeed: 2000,
 		speed: 500,
-		cssEase: 'linear'
+		cssEase: 'linear',
+		appendArrows: $('#heroArrows')
 	});
 
-	$('.multiple').slick({
+	// releases carousel
+	$('.multiple1').slick({
 		dots: false,
 		arrows: true,
 		infinite: true,
@@ -20,18 +45,33 @@ $(document).ready(function() {
 		autoplaySpeed: 4000,
 		speed: 500,
 		cssEase: 'linear',
-		appendArrows: $('.arrows')
+		appendArrows: $('.arrows1')
 	});
 
+	// review carousel
 	$('#reviews').slick({
 		dots: true,
 		arrows: false,
 		infinite: true,
-		variableWidth: true,
 		adaptiveHeight: true,
 		autoplay: true,
 		autoplaySpeed: 4000,
 		speed: 500,
-		cssEase: 'linear'
+		cssEase: 'linear',
+		appendDots: $('.reviewDots')
+	});
+
+	// blog carousel
+	$('.multiple2').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 3,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		speed: 500,
+		cssEase: 'linear',
+		appendArrows: $('.arrows2')
 	});
 });
